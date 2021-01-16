a50501a67f76b3caac117ceea06d8ff74fb09f02
Hernan Oliva <hhernanoliva@gmail.com>
2021-01-15 22:14:11 -0300

    maqueta

--- app.js
+++ app.js
@@ -1,20 +1,10 @@
 const load = () => {
   var glideMulti = new Glide('.slider', {
     type: 'slider',
-    autoplay: 3000,
+    autoplay: 2000,
     focusAt: 'center',
     perView: 1,
     gap: 0
   });
   glideMulti.mount();
-
-  var glideClases = new Glide('.slider-clases', {
-    type: 'slider',
-    autoplay: 3000,
-    focusAt: 'center',
-    perView: 1,
-    gap: 0
-  });
-  glideClases.mount();
-
 }
